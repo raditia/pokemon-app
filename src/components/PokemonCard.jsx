6824e0d5494e8b4bb9cd0730ddfd9a18d7505b28
Raditia Madya <gustiraditia@gmail.com>
2021-04-07 16:36:08 +0700

    styling pokemon card and add new detail

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,13 +3,15 @@ import styled from 'styled-components'
 import config from '../config'
 
 import './PokemonCard.css'
+import icons from '../components/Icon'
 
 const ListItemContainer = styled.div`
   background: #fff;
   border-radius: 16px;
   box-shadow: 0 0 24px -4px rgba(0,0,0,.12);
   padding: 16px;
-  max-width: 500px;
+  width:250px;
+  height:312px;
   margin-top: 16px;
   cursor: pointer
 `
@@ -18,11 +20,30 @@ const pokemonImage = (id) => {
   return config.getApiImagePath(config.api.pokemon_image(id))
 }
 
+const pokemonType = (type) => {
+  return icons[type]
+}
+
 
 const PokemonCard = ({ pokemon, onItemPressed }) => (
    <ListItemContainer onClick={ () => onItemPressed(pokemon.name) }>
-     <img src={pokemonImage(pokemon.id)} alt="" />
-     <div>{pokemon.name}</div>
+     <img className="image" src={pokemonImage(pokemon.id)} alt="" />
+     <div>
+       <div className="pokemon-name">{pokemon.name.toUpperCase()}</div>
+       { pokemon.types.map( (type, index) =>
+         <img className="type-icon" src={pokemonType(type.type.name)} alt={type.type.name} key={index}/>
+       ) }
+     </div>
+     <div className="flex-container">
+       <div>
+         <div>Height</div>
+         {pokemon.height} dm
+       </div>
+       <div>
+         <div>Weight</div>
+         <div>{pokemon.weight} hg</div>
+       </div>
+     </div>
    </ListItemContainer>
 )
 
