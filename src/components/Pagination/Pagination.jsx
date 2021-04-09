299dcb3e5a8de3e903de5c174f360ee61b75c931
Raditia Madya <gustiraditia@gmail.com>
2021-04-09 08:52:41 +0700

    update pagination styling

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import './Pagination.scss'
 
 const Pagination = ({ next, previous, onPaginationPressed, }) => {
   return (
     <div>
-      { previous ? <button type="button" onClick={() => onPaginationPressed(previous)}>previous</button> : null }
-      <button type="button" onClick={() => onPaginationPressed(next)}>next</button>
+      { previous
+        ? <button type="button" className="pagination-btn" onClick={() => onPaginationPressed(previous)}>previous</button>
+        : null }
+      <button type="button" className="pagination-btn" onClick={() => onPaginationPressed(next)}>next</button>
     </div>
   )
 }
